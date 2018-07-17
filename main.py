c36f260209fe1298df1477b09b70071e549be708
Finn Pauls <derfinn@gmail.com>
2018-07-17 13:35:52 +0200

    Start with mondrian

--- main.py
+++ main.py
@@ -4,53 +4,68 @@ import imutils
 import os
 import json
 import datetime
-from find_tickets import find_tickets
 
 retr_type = cv2.RETR_LIST
 contour_algorithm = cv2.CHAIN_APPROX_SIMPLE
 
 input_dir = 'img'
 output_dir = 'output'
-border = 30
 image_type = 'jpg'
 
-def calculate_distance(x1, y1, x2, y2):
-    return (x2 - x1)**2 + (y2 - y1)**2
+threshold_value = 50
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(10,10))
+
+def find_contours (img):
+    _, contours, _ = cv2.findContours(img.copy(), retr_type, contour_algorithm)
+    contours = list(filter(lambda cont: cv2.contourArea(cont) > min_ticket_size, contours))
+    rects = []
+    polygons = []
+    for cont in contours:
+        polygon = cv2.approxPolyDP(cont, 40, True).copy().reshape(-1, 2)
+        polygon = cv2.convexHull(polygon)
+        if (len(polygon) > 15): continue # possibly not needed when comparing the areas
+        area = cv2.contourArea(polygon)
+        rect = cv2.boundingRect(polygon)
+        x,y,width,height = rect
+        if (width > 2.3*height or height > 2.3*width): continue # unusual shape
+        rect_area = width * height
+        area_diff = abs(rect_area - area)
+        if (area_diff > 60000): continue
+        rects.append(rect)
+        polygons.append(polygon)
+
+    return (rects, polygons)
 
 def process_image(file_name):
     original = cv2.imread(input_dir + '/' + file_name)
 
-    tickets = find_tickets(original)
+    b,g,r = cv2.split(original)
+    _, binary = cv2.threshold(original, threshold_value, 255, cv2.THRESH_BINARY)
 
-    # remove probable duplicates TODO: move to tickets.py
-    for ticket_a in tickets:
-        x1, y1, w1, h1 = ticket_a
-        for ticket_b in tickets:
-            x2, y2, w2, h2 = ticket_b
-            distance = calculate_distance(x1, y1, x2, y2)
-            if (distance < 10000):
-                tickets.remove(ticket_b)
+    b_t,g_t,r_t = cv2.split(binary)
 
-    print(len(tickets), 'tickets found')
-    for index, ticket in enumerate(tickets):
-        x,y,w,h = ticket
-        cropped = original[y - border: y + h + border, x - border: x + w + border]
-        metadata = {
-            'x': x,
-            'y': y,
-            'width': w,
-            'height': h,
-            'tableau': file_name,
-            'created_at': datetime.datetime.now().isoformat()
-        }
+    max = cv2.max(cv2.max(b_t, g_t), r_t)
 
-        file_without_ending = file_name[:-len('.' + image_type)]
+    opening = cv2.morphologyEx(max, cv2.MORPH_OPEN, kernel)
 
-        output_filename = output_dir + '/' + file_without_ending + '-' + str(index)
 
-        with open(output_filename + '.json', 'w') as fp:
-            json.dump(metadata, fp)
-        cv2.imwrite(output_filename  + '.jpg', cropped)
+    file_without_ending = file_name[:-len('.' + image_type)]
+    output_filename = output_dir + '/' + file_without_ending + '-r'
+    cv2.imwrite(output_filename  + '.jpg', r)
+    output_filename = output_dir + '/' + file_without_ending + '-g'
+    cv2.imwrite(output_filename  + '.jpg', g)
+    output_filename = output_dir + '/' + file_without_ending + '-b'
+    cv2.imwrite(output_filename  + '.jpg', b)
+    output_filename = output_dir + '/' + file_without_ending + '-binary'
+    cv2.imwrite(output_filename  + '.jpg', binary)
+    output_filename = output_dir + '/' + file_without_ending + '-max'
+    cv2.imwrite(output_filename  + '.jpg', max)
+    output_filename = output_dir + '/' + file_without_ending + '-open'
+    cv2.imwrite(output_filename  + '.jpg', opening)
+    #
+    #     with open(output_filename + '.json', 'w') as fp:
+    #         json.dump(metadata, fp)
 
 files = os.listdir(input_dir)
 files = list(filter(lambda f: f[-len(image_type):] == 'jpg', files))
