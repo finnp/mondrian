2ca974fd99154411c21f581a120761a996372654
Finn Pauls <derfinn@gmail.com>
2018-08-23 21:01:24 +0200

    Download images from RKD

---
+++ download-rkd.py
@@ -0,0 +1,22 @@
+"""
+    Downloads images from RKD listed in the catalogue
+"""
+
+import os
+import json
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, urlretrieve
+
+dir = 'catalogue'
+files = os.listdir(dir)
+
+for file in files:
+    with open(dir + '/' + file) as f:
+        data = json.load(f)
+        page = urlopen(data['database'])
+        id = data['id']
+        soup = BeautifulSoup(page, 'html.parser')
+        thumb_id = soup.select('.record-metadata')[0]['data-thumb-id']
+        download_url = 'https://images.rkd.nl/rkd/thumb/1000x1000/' + thumb_id + '.jpg'
+        urlretrieve(download_url, 'img/' + id + '.jpg')
+        print('File ' + file + 'downloaded.')
--- scrape.py
+++ scrape.py
@@ -15,14 +15,19 @@ cr_id_regex = re.compile('B\d+')
 for div in soup.select('table.main'):
     img = div.select('img')[0]
     text = div.select('td[align=left]')[0].text
+    share = div.select('.addthiscontainer')[0]
+    direct_link = share['addthis:url']
+    database_link = div.select('.database-link')[0]['href']
     cr_id = cr_id_regex.search(text).group(0)
 
     file_name = 'catalogue/' + cr_id + '.json'
     output = open(file_name, 'w')
     output.write(json.dumps({
         'id': cr_id,
+        'database': database_link,
+        'link': direct_link,
         'thumbnail': img['src'],
-        'description': text
+        'description': text,
     }, indent=2))
     output.close()
     print('Written ' + file_name)
