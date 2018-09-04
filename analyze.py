d1235a867f7efead43b368ca4205783c80aec5d7
Finn Pauls <derfinn@gmail.com>
2018-09-05 00:53:54 +0200

    Distribution of colors

---
+++ analyze.py
@@ -0,0 +1,49 @@
+import json, os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dir = 'detected'
+out_dir = 'graphs'
+
+files = os.listdir(dir)
+
+def get_color_distribution(data):
+    full_area = float(data['height'] * data['width'])
+    colors = {
+        'black': 0,
+        'white': 0,
+        'blue': 0,
+        'yellow': 0,
+        'red': 0
+    }
+    for rect in data['rectangles']:
+        rect_area = rect['height'] * rect['width']
+        colors[rect['color_id']] += float(rect_area) / full_area
+    return colors
+
+color_distribution_by_area = []
+
+for file in files:
+    print(file)
+    with open(dir + '/' + file) as f:
+        data = json.load(f)
+    by_area = get_color_distribution(data)
+    total = sum(by_area.values())
+    if (total > 1.1 or total < 0.9):
+        print('Problem with ' + file)
+    color_distribution_by_area.append(by_area)
+
+df = pd.DataFrame(color_distribution_by_area)
+df.boxplot()
+plt.savefig(out_dir + '/colors.png')
+plt.close()
+
+axes = df.boxplot(column=['red', 'blue', 'yellow'], return_type='axes')
+axes.set_ylim(0, 0.3)
+plt.savefig(out_dir + '/colors-rby.png')
+
+df['colors'] = df['red'] + df['blue'] + df['yellow']
+df['non-colors'] = df['white'] + df['black']
+plt.close()
+df.boxplot(column=['colors', 'non-colors'])
+plt.savefig(out_dir + '/colors-non-colors.png')
