4f6b68457250d3456f8dbe5d78fe8e282bc3895f
Finn Pauls <derfinn@gmail.com>
2018-09-17 20:44:56 +0200

    Save that shit

---
+++ getpaintinginfos.py
@@ -0,0 +1,6 @@
+from files import read_data
+
+with open('paintings.txt') as f:
+    for cnt, line in enumerate(f):
+        data = read_data('catalogue/' + line[:-1] + '.json')
+        print(data['year'])
